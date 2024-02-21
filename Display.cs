2abc8753e2f2910ef33caeecb1245cd4418b31be
robomenRmasus <alex@robomen.net>
2024-02-21 13:23:18 +0200

    Display first

---
+++ Display.cs
@@ -0,0 +1,40 @@
+using System;
+
+public class Display
+{
+	public void Run()
+	{
+	string[] number = {"1", "2", "3", "4", "5", "6", "7", "8","9", "10", "11", "12", "13", "14", "15",};
+		int[] cube = new int[15];
+		//Start(cube);
+		//Display(number, cube);
+	}
+	public void DisplayA(string[] number, int[] cube) 
+	{
+		
+		//Визуална репрезентация за Пъзел 15
+		
+		Console.WriteLine("---------------------------");
+		Start(cube);
+		Console.WriteLine("|  " + number[cube[0]] + "  |  " + number[cube[1]] + "  |  " + number[cube[2]] + "  |  " + number[cube[3]] + "  |");
+		Console.WriteLine("---------------------------");
+		Start(cube); 
+		Console.WriteLine("|  " + number[cube[4]] + "  |  " + number[cube[5]] + "  |  " + number[cube[6]] + "  |  " + number[cube[7]] + "  |");
+		Console.WriteLine("---------------------------");
+		Console.WriteLine("|  " + number[cube[8]] + "  |  " + number[cube[9]] + "  |  " + number[cube[10]] + "  |  " + number[cube[11]] + "  |");
+		Console.WriteLine("---------------------------");
+		Start(cube);
+		Console.WriteLine("|  " + number[cube[12]] + "  |  " + number[cube[13]] + "  |  " + number[cube[14]] + "  |  " + " " + "  |");
+		Console.WriteLine("---------------------------");
+	}
+
+	public void Start(int[] chisla)
+	{
+		Random rnd = new Random();
+		for (int i = 0; i < 15; i++)
+		{
+			chisla[i] = rnd.Next(0, 15);
+		//Console.WriteLine(chisla); 
+		}
+	}
+}
