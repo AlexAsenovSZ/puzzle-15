c717578d18ad68b121afa64c1a515f80ef57cf45
robomenRmasus <alex@robomen.net>
2024-02-21 13:32:30 +0200

    DisplayB

--- Display.cs
+++ Display.cs
@@ -27,6 +27,33 @@ public class Display
 		Console.WriteLine("|  " + number[cube[12]] + "  |  " + number[cube[13]] + "  |  " + number[cube[14]] + "  |  " + " " + "  |");
 		Console.WriteLine("---------------------------");
 	}
+	
+	public static void DisplayB(string[] number, int[] cube) 
+	{
+		
+		//Визуална репрезентация за Пъзел 15
+		
+		Console.WriteLine("-----------------------------------");
+		Start(cube);
+		Console.WriteLine("|  " + " " + " " + " " + "  |  " + " " + " " + " " + "  |  " + " " + " " + " " + "  |  " + " " + " " + "  |");
+		Console.WriteLine("|  " + " " + number[cube[0]] + " " + "  |  " + " " + number[cube[1]] + " " + "  |  " + " " + number[cube[2]] + " " + "  |  " + number[cube[3]] + " " + "  |");
+		Console.WriteLine("|  " + " " + " " + " " + "  |  " + " " + " " + " " + "  |  " + " " + " " + " " + "  |  " + " " + " " + "  |");
+		Console.WriteLine("-----------------------------------");
+		Start(cube); 
+		Console.WriteLine("|  " + " " + " " + " " + "  |  " + " " + " " + " " + "  |  " + " " + " " + " " + "  |  " + " " + " " + "  |");
+		Console.WriteLine("|  " + " " + number[cube[4]] + " " + "  |  " + " " + number[cube[5]] + " " + "  |  " + " " + number[cube[6]] + " " + "  |  " + number[cube[7]] + " " + "  |");
+		Console.WriteLine("|  " + " " + " " + " " + "  |  " + " " + " " + " " + "  |  " + " " + " " + " " + "  |  " + " " + " " + "  |");
+		Console.WriteLine("-----------------------------------");
+		Console.WriteLine("|  " + " " + " " + " " + "  |  " + " " + " " + " " + "  |  " + " " + " " + " " + "  |  " + " " + " " + "  |");
+		Console.WriteLine("|  " + " " + number[cube[8]] + " " + "  |  " + " " + number[cube[9]] + " " + "  |  " + " " + number[cube[10]] + " " + "  |  " + number[cube[11]] + " " + "  |");
+		Console.WriteLine("|  " + " " + " " + " " + "  |  " + " " + " " + " " + "  |  " + " " + " " + " " + "  |  " + " " + " " + "  |");
+		Console.WriteLine("-----------------------------------");
+		Start(cube);
+		Console.WriteLine("|  " + " " + " " + " " + "  |  " + " " + " " + " " + "  |  " + " " + " " + " " + "  |  " + " " + " " + "  |");
+		Console.WriteLine("|  " + " " + number[cube[12]] + " " + "  |  " + " " + number[cube[13]] + " " + "  |  " + " " + number[cube[14]] + " " + "  |  " + " " + "  |");
+		Console.WriteLine("|  " + " " + " " + " " + "  |  " + " " + " " + " " + "  |  " + " " + " " + " " + "  |  " + " " + " " + "  |");
+		Console.WriteLine("-----------------------------------");
+	}
 
 	public void Start(int[] chisla)
 	{
